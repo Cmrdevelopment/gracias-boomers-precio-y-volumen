bb6e1c0a7be04609ec03c329f597ec15448eec28
Cmrdeveloper <cmrbolsadevelopment@gmail.com>
2023-12-10 17:20:35 +0000

    inicio página gracias al inscribirte

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import GraciasWeb from "./components/GraciasWeb.jsx";
+
+
+const App = () => {
+  
+
+  return (
+    <>
+      <GraciasWeb/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/GraciasWeb.jsx
@@ -0,0 +1,15 @@
+
+
+const GraciasWeb = () => {
+
+
+
+    return (
+        <h1>
+            Hola MUNDO
+        </h1>
+    );
+
+};
+
+export default GraciasWeb;
